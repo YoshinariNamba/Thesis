876f164afa93c8c68eb6817efbfbed160e253fbf
YoshinariNamba <nyoshi6737@gmail.com>
2021-11-24 19:11:45 +0900

    [change dir structure and japic append]

---
+++ analysis/dataset/japic/append/const.R
@@ -0,0 +1,170 @@
+library(tidyverse)
+library(magrittr)
+
+# directory
+setwd("C:/Users/Namba Yoshinari/Documents/Github_Repositories/Thesis/analysis/dataset/japic/append")
+
+# construct ---------------------------------------------------------------
+
+## 2015
+### read first file
+df_jpc_15 <- read.delim("../201507/h1.txt")
+
+### loop reading and merging 
+for(i in 2:9){
+  assign("df_tmp", 
+         read.delim(paste0("../201507/h", i, ".txt"))) 
+  df_jpc_15 <- df_jpc_15 %>% 
+    rbind(df_tmp)
+}
+rm(df_tmp)
+
+### add "year"
+df_jpc_15 <- df_jpc_15 %>% 
+  mutate(year = 2015) %>% 
+  select(ncol(.), 1:(ncol(.)-1))
+
+
+
+## 2016
+### read first file
+df_jpc_16 <- read.delim("../201607/h1.txt")
+
+### loop reading and merging 
+for(i in 2:9){
+  assign("df_tmp", 
+         read.delim(paste0("../201607/h", i, ".txt"))) 
+  df_jpc_16 <- df_jpc_16 %>% 
+    rbind(df_tmp)
+}
+rm(df_tmp)
+
+### add "year"
+df_jpc_16 <- df_jpc_16 %>% 
+  mutate(year = 2016) %>% 
+  select(ncol(.), 1:(ncol(.)-1))
+
+
+
+
+## 2017
+### read first file
+df_jpc_17 <- read.delim("../201707/h1.txt")
+
+### loop reading and merging 
+for(i in 2:9){
+  assign("df_tmp", 
+         read.delim(paste0("../201707/h", i, ".txt"))) 
+  df_jpc_17 <- df_jpc_17 %>% 
+    rbind(df_tmp)
+}
+rm(df_tmp)
+
+### add "year"
+df_jpc_17 <- df_jpc_17 %>% 
+  mutate(year = 2017) %>% 
+  select(ncol(.), 1:(ncol(.)-1))
+
+
+
+
+## 2018
+### read first file
+df_jpc_18 <- read.delim("../201807/h1.txt")
+
+### loop reading and merging 
+for(i in 2:9){
+  assign("df_tmp", 
+         read.delim(paste0("../201807/h", i, ".txt"))) 
+  df_jpc_18 <- df_jpc_18 %>% 
+    rbind(df_tmp)
+}
+rm(df_tmp)
+
+### add "year"
+df_jpc_18 <- df_jpc_18 %>% 
+  mutate(year = 2018) %>% 
+  select(ncol(.), 1:(ncol(.)-1))
+
+
+## 2019
+### read first file
+df_jpc_19 <- read.delim("../201907/h1.txt")
+
+### loop reading and merging 
+for(i in 2:9){
+  assign("df_tmp", 
+         read.delim(paste0("../201907/h", i, ".txt"))) 
+  df_jpc_19 <- df_jpc_19 %>% 
+    rbind(df_tmp)
+}
+rm(df_tmp)
+
+### add "year"
+df_jpc_19 <- df_jpc_19 %>% 
+  mutate(year = 2019) %>% 
+  select(ncol(.), 1:(ncol(.)-1))
+
+
+
+
+## 2020
+### read first file
+df_jpc_20 <- read.delim("../202007/h1.txt")
+
+### loop reading and merging 
+for(i in 2:9){
+  assign("df_tmp", 
+         read.delim(paste0("../202007/h", i, ".txt"))) 
+  df_jpc_20 <- df_jpc_20 %>% 
+    rbind(df_tmp)
+}
+rm(df_tmp)
+
+### add "year"
+df_jpc_20 <- df_jpc_20 %>% 
+  mutate(year = 2020) %>% 
+  select(ncol(.), 1:(ncol(.)-1))
+
+
+
+
+## 2021
+### read first file
+df_jpc_21 <- read.delim("../202107/h1.txt")
+
+### loop reading and merging 
+for(i in 2:9){
+  assign("df_tmp", 
+         read.delim(paste0("../202107/h", i, ".txt"))) 
+  df_jpc_21 <- df_jpc_21 %>% 
+    rbind(df_tmp)
+}
+rm(df_tmp)
+
+### add "year"
+df_jpc_21 <- df_jpc_21 %>% 
+  mutate(year = 2021) %>% 
+  select(ncol(.), 1:(ncol(.)-1))
+
+
+
+# append ------------------------------------------------------------------
+
+## glimpse
+list_tmp <- list(df_jpc_15, df_jpc_16, df_jpc_17, df_jpc_18, 
+                 df_jpc_19, df_jpc_20, df_jpc_21)
+for(i in 1:length(2015:2019)){
+  print(colnames(list_tmp[[i]]))
+}
+rm(list_tmp)
+
+
+## merge
+df_jpc <- rbind(df_jpc_15, df_jpc_16, df_jpc_17, df_jpc_18, 
+                df_jpc_19, df_jpc_20, df_jpc_21)
+rm(list = c("df_jpc_15", "df_jpc_16", "df_jpc_17", "df_jpc_18", 
+            "df_jpc_19", "df_jpc_20", "df_jpc_21"))
+
+
+
---
+++ analysis/dataset/japic/append/glimpse.R
@@ -0,0 +1,19 @@
+# library
+library(tidyverse)
+library(magrittr)
+
+# directory
+setwd("C:/Users/Namba Yoshinari/Documents/Github_Repositories/Thesis/analysis/dataset/japic/append")
+
+# source
+source("const.R")
+
+# summary
+df_jpc %>% 
+  summary()
+
+
+# 
+df_jpc %>% 
+  select(2) %>% 
+  na.omit()
